90ec8e67442b495ddb4f97426c79e92f728c583c
pllll44 <pllll44@bk.ru>
2026-05-06 23:07:07 +0800

    Выполнил лабораторную работу №3.2

---
+++ 3.2_operator_foreach/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3._2_operator_foreach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Лабораторная работа 3.2 Оператор foreach
+
+            //Синтаксис
+
+            //foreach( элемент in in итерируемый объект) 
+            //{
+            //Тело цикла
+            //}
+
+            //Пример 1 Вывод элементов массива с помощью оператора for
+            //char[] array = {"H","e","l","l","o","W","o","r","l","d"};
+
+            //for (int i = 0; i < array.Lenght; i++)
+            //{
+            //Console.Writeline(array[i]);
+            //}
+
+            //Пример 2 Вывод элементов массива с помощью оператора foreach
+            //foreach (char ch in array)
+            //{
+            //Console.Writeline(ch);
+            //}
+
+            //Пример 3 Перебор массива, содержащего данные о поле соискателя
+            //char [] gender = {'м','ж','м','ж','м','ж','м','ж','м','ж',};
+            //int mans = 0,womens = 0;
+
+            //foreach (char g in gender)
+            //{
+            //if (g == 'м')
+            //mans++;
+            //else 
+            //womens++;
+
+            //}
+
+            //Console.Writeline("Количество мужчин ровно: {0}", mans);
+            //Console.Writeline("Количество женщин ровно: {0}", womens);
+
+            //Пример 4 работа цикла foreach сщ списком
+            // Сума элементов списка
+
+            //var numbers = new list<int> {5,-8,2,15,9,17,0,4};
+
+            //int sum = 0;
+
+            //foreach (int num in numbers) 
+            //{
+            //sum+=num;
+            //}
+            //Console.Writeline(sum);
+        }
+    }
+}
---
+++ 3.2_operator_foreach/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("3.2_operator_foreach")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Image&Matros ®")]
+[assembly: AssemblyProduct("3.2_operator_foreach")]
+[assembly: AssemblyCopyright("Copyright © Image&Matros ® 2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("16ebee9e-82ed-4430-a272-d00456d29873")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
